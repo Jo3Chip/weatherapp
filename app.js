65f6ad415222218245968475c88b998a53695051
Joseph Chipperfield <joechip1709@gmail.com>
2021-07-24 16:20:36 +0100

    Data updating page successfully

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
 var locationInput = document.getElementById("location");
 var units = '&units=metric'
 var apiKey = '&appid=3f265935a23a9aeba683d087c268057d'
+var weatherDisplay = document.getElementById('weatherDisplay');
+var weatherData;
+
 
 
 function search() {
@@ -18,8 +21,16 @@ function search() {
             return response.json();
         }
     })
-    .then(data => console.log(data))
+    .then(data => weatherData = data)
+    .then(() => console.log(weatherData))
+    .then(() => update())
     .catch(error => console.log('ERROR'))
+    
+}
+
+function update() {
+    weatherDisplay.innerHTML = weatherData.weather[0].description;
+    
 }
 
 submit.addEventListener('click', () => {
