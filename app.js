6ebcde4e35aa1cad86e30bab1a1834832720c8ac
Joseph Chipperfield <joechip1709@gmail.com>
2021-07-24 15:40:13 +0100

    api working using fetch with failure alerts in place

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-console.log('about to fetch the weather');
-fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=3f265935a23a9aeba683d087c268057d')
-.then(response => {
-    return response.json;
+var submit = document.getElementById('submit');
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+var locationInput = document.getElementById("location");
+var units = '&units=metric'
+var apiKey = '&appid=3f265935a23a9aeba683d087c268057d'
+
+
+function search() {
+    var url = baseURL + locationInput.value + units + apiKey;
+    console.log(url);
+    fetch(url)
+    .then(response => {
+        if(!response.ok){
+            alert('Please Select Another Location');
+        }
+        else {
+            console.log('Successful');
+            return response.json();
+        }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+}
+
+submit.addEventListener('click', () => {
+    search();
 })
-.then(response => {
-    console.log(response);
-});
+
