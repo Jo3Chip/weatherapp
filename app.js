ef6c197e9261ecf6f8d6a8922a7f7fe6af5bc52c
Joseph Chipperfield <joechip1709@gmail.com>
2021-08-04 22:49:32 +0100

    Project completed

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var weatherDisplay = document.getElementById('description');
 var weatherMain = document.getElementById('main');
 var weatherTemp = document.getElementById('temp');
 var container = document.getElementById('full-container');
+var locationName = document.getElementById('locationName');
+var windSpeed = document.getElementById('wind');
 var weatherData;
 //Array of background images that relate to weather title returned from the API (all matching the current documentation (03/08/21))
 var background = {
@@ -32,39 +34,48 @@ var background = {
 //API call function
 function search() {
     var url = baseURL + locationInput.value + units + apiKey;
-    console.log(url);
+    //console.log(url);
     fetch(url)
     .then(response => {
         if(!response.ok){
             alert('Please Select Another Location');
         }
         else {
-            console.log('Successful');
+            //console.log('Successful');
             return response.json();
         }
     })
     .then(data => weatherData = data)
-    .then(() => console.log(weatherData))
+    //.then(() => console.log(weatherData))
     .then(() => update())
     .then(() => backgroundChange())
     .catch(error => console.log('ERROR'))
     
 }
+//Function to caplitalize the first letter of the location and return as a string
+function capitalizeFirstLetter(){
+    var input = locationInput.value;
+    return input.charAt(0).toUpperCase() + input.slice(1);
+}
+
+function convertwindSpeed(){
+    var speed = weatherData.wind.speed;
+    return (speed*2.237).toFixed();
+}
+
 //Updates the information on the UI elements
 function update() {
-    weatherMain.innerHTML = weatherData.weather[0].main;
-    weatherDisplay.innerHTML = weatherData.weather[0].description;
-    weatherTemp.innerHTML = weatherData.main.temp;
+    locationName.innerHTML = capitalizeFirstLetter();
+    weatherDisplay.innerHTML = "Current Weather: " + weatherData.weather[0].description;
+    weatherTemp.innerHTML = "Current Temp: " + weatherData.main.temp + "°C";
+    windSpeed.innerHTML = "Current Wind Speed: " + convertwindSpeed() + " MPH";
 }
 //Changes the background depending on the title taken from the API.
 function backgroundChange() {
     for (var key in background){
-        if(weatherMain.innerHTML == key){
+        if(weatherData.weather[0].main == key){
             container.style.backgroundImage = background[key]
-            console.log("key " + key + " has value " + background[key]);
-        }
-        else{
-            console.log('weather not found');
+            //console.log("key " + key + " has value " + background[key]);
         }
     }
 }
@@ -78,4 +89,5 @@ locationInput.addEventListener('keypress', function(e) {
         search();
     }
 })
-
+// remove this after completing styling
+search();
